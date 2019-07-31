06d6be6300c09613afffb00463bcc981898081d1
Ruiyu <ruiyuwu1998@gmail.com>
2019-07-30 23:03:25 -0400

    Fix logout

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -27,7 +27,7 @@ export const login = user => dispatch => {
 
 export const logout = () => dispatch => {
     return SessionAPIUtil.logout()
-                        .then (() => dispatch(logout()),
+                        .then (() => dispatch(logoutCurrentUser()),
                         errors => dispatch(receiveSessionErrors(errors)))
 }
 
