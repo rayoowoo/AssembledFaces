6b910f3826a42e7db3d711ae64721f93a17b01b9
Ruiyu <ruiyuwu1998@gmail.com>
2019-08-05 00:40:25 -0400

    Make pictures of users in posts and comments links to the author's profile page

--- frontend/components/post/post_form.jsx
+++ frontend/components/post/post_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class PostForm extends React.Component {
     constructor(props) {
@@ -79,7 +80,7 @@ class PostForm extends React.Component {
                     <form onSubmit={this.handleSubmit.bind(this)} className="postinput">
                         <section className="postinput-top">
                             <div className="post-picture">
-                                <img src={`https://fsmedia.imgix.net/32/97/14/c9/033f/4ac9/a023/bbdc07fe72a0/avengers-endgame-iron-man-death-scene.png?rect=0%2C0%2C972%2C487&auto=format%2Ccompress&w=650`} alt="" />
+                                    <Link to={`/user/${this.props.currentUser.id}`}><img src={`https://fsmedia.imgix.net/32/97/14/c9/033f/4ac9/a023/bbdc07fe72a0/avengers-endgame-iron-man-death-scene.png?rect=0%2C0%2C972%2C487&auto=format%2Ccompress&w=650`} alt="" /></Link>
                                 {/* FROM 1000logos.net/iron-man-logo. All rights go to Marvel Studios. */}
                             </div>
                             <textarea onChange={this.handleChange} onFocus={this.focusForm} type="text" placeholder={`What's on your mind?`} value={this.state.body}></textarea>
--- frontend/components/post/post_item.jsx
+++ frontend/components/post/post_item.jsx
@@ -25,7 +25,7 @@ class PostItem extends React.Component {
                 <section className="postitem-top">
                     
                     <div className="post-content-picture post-picture">
-                        <img src={`https://fsmedia.imgix.net/32/97/14/c9/033f/4ac9/a023/bbdc07fe72a0/avengers-endgame-iron-man-death-scene.png?rect=0%2C0%2C972%2C487&auto=format%2Ccompress&w=650`} alt="" />
+                        <Link to={`/user/${this.props.author.id}`}><img src={`https://fsmedia.imgix.net/32/97/14/c9/033f/4ac9/a023/bbdc07fe72a0/avengers-endgame-iron-man-death-scene.png?rect=0%2C0%2C972%2C487&auto=format%2Ccompress&w=650`} alt="" /></Link>
                         {/* FROM 1000logos.net/iron-man-logo. All rights go to Marvel Studios. */}
                     </div>
                     <div className="post-content">
