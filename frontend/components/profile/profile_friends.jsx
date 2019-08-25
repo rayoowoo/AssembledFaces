197caa3a7635a3730c8be2b5e4783979cacc15e8
Ruiyu <ruiyuwu1998@gmail.com>
2019-08-25 17:02:43 -0400

    Start friends page

--- frontend/components/profile/profile_friends.jsx
+++ frontend/components/profile/profile_friends.jsx
@@ -1,2 +1,15 @@
 import React from 'react';
 
+class ProfileFriends extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>HI</h1>
+        )
+    }
+}
+
+export default ProfileFriends;
