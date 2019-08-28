acb4310840750371d40a800d250436a95950c1f1
Ruiyu <ruiyuwu1998@gmail.com>
2019-08-27 23:28:59 -0400

    Add portofilio site

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -20,6 +20,7 @@ const Splash = (props) => {
             <section className="footer"><a href="https://github.com/rayoowoo/AssembledFaces" target="_blank">GITHUB</a>
                                         <a href="https://www.linkedin.com/in/ruiyu-wu-173604134/" target="_blank">LINKEDIN</a>
                                         <a href="https://angel.co/ruiyu-wu" target="_blank">ANGELLIST</a>
+                                        <a href="https://rayoowoo.github.io/rayoowoo" target="_blank">PORTFOLIO</a>
                                         <a href="mailto: ruiyuwu1998@gmail.com" target="_blank">EMAIL</a>
                                         </section>
             <section className="citation">Made by Ruiyu Wu. Inspired by Facebook and Marvel Studios.</section>
